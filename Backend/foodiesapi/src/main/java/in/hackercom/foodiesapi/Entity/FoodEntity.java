0c7f56c0c1d0c9f1ef6ef1fafeb01b051c0be01f
ANKIT KUMAWAT <ankitkumawat0602@gmail.com>
2025-07-12 21:22:40 +0530

    EntityUpdated

--- Backend/foodiesapi/src/main/java/in/hackercom/foodiesapi/Entity/FoodEntity.java
+++ Backend/foodiesapi/src/main/java/in/hackercom/foodiesapi/Entity/FoodEntity.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collation = "Foods")
+@Document(collection = "Foods")
 public class FoodEntity {
     @Id
     private String id;
