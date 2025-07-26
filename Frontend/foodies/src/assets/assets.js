4cff9742efc56e79e08e0b061d1024d7527a8e3a
ANKIT KUMAWAT <ankitkumawat0602@gmail.com>
2025-07-26 21:38:35 +0530

    Navbar Added

---
+++ Frontend/foodies/src/assets/assets.js
@@ -0,0 +1,11 @@
+import FoodieFood from './FoodieFood.png'
+import parcel from './parcel.png'
+import upload from './upload.png'
+import cart from './cart.png'
+
+export const assets = {
+    FoodieFood,
+    parcel,
+    upload,
+    cart
+};
